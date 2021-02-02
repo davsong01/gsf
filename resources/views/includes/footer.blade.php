62bd0b5a35cf41f2e08b894aac560209ff1edb5f
kennietimmie <kehinde.bridgewateracademy@gmail.com>
2021-02-02 23:26:01 +0100

    select2 js/css

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,59 +3,71 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="footer-copy-right">
-                    <p>Copyright &#169; {{  date('Y') }}. All rights reserved.<a href="#">Built by H. Pierson Associates I.T. Team</a>.</p>
-                    {{-- Build Credit: David Oghi, Abraham Ajiboye and Kehinde S. --}}
-                {{-- </div> --}}
-            {{-- </div> --}}
-        {{-- </div> --}}
-    {{-- </div> --}}
+                    <p>Copyright &#169; {{  date('Y') }}. All rights reserved.<a href="#">Built by H. Pierson
+	Associates I.T. Team</a>.</p>
+{{-- Build Credit: David Oghi, Abraham Ajiboye and Kehinde S. --}}
 {{-- </div> --}}
-    <footer class="footer footer-static footer-light">
-            <p class="clearfix mb-0"><span class="float-left d-inline-block"></span><span class="float-right d-sm-inline-block d-none">&copy; {{ date('Y') }} GSF | Built by<a class="text-uppercase" href="https://techdaves.com/" target="_blank">Techdaves</a></span>
-                <button class="btn btn-primary btn-icon scroll-top" type="button"></button>
-            </p>
-    </footer>
+{{-- </div> --}}
+{{-- </div> --}}
+{{-- </div> --}}
+{{-- </div> --}}
+<footer class="footer footer-static footer-light">
+	<p class="clearfix mb-0"><span class="float-left d-inline-block"></span><span
+			class="float-right d-sm-inline-block d-none">&copy; {{ date('Y') }} GSF | Built by<a class="text-uppercase"
+				href="https://techdaves.com/" target="_blank">Techdaves</a></span>
+		<button class="btn btn-primary btn-icon scroll-top" type="button"></button>
+	</p>
+</footer>
 <!-- BEGIN: Vendor JS-->
-    <script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/ui/prism.min.js')}}"></script>
+<script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/ui/prism.min.js')}}"></script>
 
-    <script src="{{ asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js') }}"></script>
-    <script src="{{ asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js') }}"></script>
-    <script src="{{ asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js') }}"></script>
-    <!-- BEGIN Vendor JS-->
+<script src="{{ asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js') }}"></script>
+<script src="{{ asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js') }}"></script>
+<script src="{{ asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js') }}"></script>
+<!-- BEGIN Vendor JS-->
 
-    <!-- BEGIN: Page Vendor JS-->
-    <script src="{{ asset('app-assets/vendors/js/charts/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/extensions/swiper.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
-    {{-- <script src="{{ asset('app-assets/vendors/js/editors/quill/katex.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/editors/quill/highlight.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/editors/quill/quill.min.js') }}"></script> --}}
-    <script src="{{ asset('app-assets/vendors/js/extensions/jquery.steps.min.js') }}"></script>
-    <script src="{{ asset('app-assets/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>
-    
-    <script src="{{ asset('app-assets/vendors/js/ui/prism.min') }}"></script>
- 
-    <!-- END: Page Vendor JS-->
+<!-- BEGIN: Page Vendor JS-->
+<script src="{{ asset('app-assets/vendors/js/charts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/extensions/swiper.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
+{{-- <script src="{{ asset('app-assets/vendors/js/editors/quill/katex.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/editors/quill/highlight.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/editors/quill/quill.min.js') }}"></script> --}}
+<script src="{{ asset('app-assets/vendors/js/extensions/jquery.steps.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>
 
-    <!-- BEGIN: Theme JS-->
-    <script src="{{ asset('app-assets/js/scripts/configs/vertical-menu-light.js') }}"></script>
-    <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
-    <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
-    <script src="{{ asset('app-assets/js/scripts/components.js') }}"></script>
-    <script src="{{ asset('app-assets/js/scripts/footer.js') }}"></script>
-    <!-- END: Theme JS-->
+<script src="{{ asset('app-assets/vendors/js/ui/prism.min') }}"></script>
 
-    <!-- BEGIN: Page JS-->
-    <script src="{{ asset('app-assets/js/scripts/pages/dashboard-ecommerce.js') }}"></script>
-    <script src="{{ asset('app-assets/js/scripts/datatables/datatable.js') }}"></script>
-    {{-- <script src="{{ asset('app-assets/js/scripts/editors/editor-quill.js') }}"></script> --}}
-    <!-- END: Page JS-->
-    <!-- END: Footer-->
+<!-- END: Page Vendor JS-->
+
+<!-- BEGIN: Theme JS-->
+<script src="{{ asset('app-assets/js/scripts/configs/vertical-menu-light.js') }}"></script>
+<script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
+<script src="{{ asset('app-assets/js/core/app.js') }}"></script>
+<script src="{{ asset('app-assets/js/scripts/components.js') }}"></script>
+<script src="{{ asset('app-assets/js/scripts/footer.js') }}"></script>
+<!-- END: Theme JS-->
+
+<!-- BEGIN: Page JS-->
+<script src="{{ asset('app-assets/js/scripts/pages/dashboard-ecommerce.js') }}"></script>
+<script src="{{ asset('app-assets/js/scripts/datatables/datatable.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+	$(document).ready(function() {
+    $('select').select2({
+    width: '100%' // need to override the changed default
+});
+});
+</script>
+{{-- <script src="{{ asset('app-assets/js/scripts/editors/editor-quill.js') }}"></script> --}}
+<!-- END: Page JS-->
+<!-- END: Footer-->
