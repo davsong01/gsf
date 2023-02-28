862ef028f5695f9b89eabe216fbd987e68891f28
David Oghi <davsong16@gmail.com>
2023-02-28 20:25:39 +0100

    minor bugs fixing

--- resources/views/frontend/conference/template1/index.blade.php
+++ resources/views/frontend/conference/template1/index.blade.php
@@ -13,7 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>Gofamint Students' Fellowship - {{ $setting->conference_theme }}</title>
+    <title>{{ config('app_name') }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
