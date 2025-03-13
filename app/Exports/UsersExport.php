da3ef6abd18885e3d472fd1e2ea011e93524b59f
David Oghi <david.oghi@broadshiftconnect.com>
2025-03-13 08:31:32 +0100

    fixed issue with conference export

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\User;
+use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
