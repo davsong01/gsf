0d66fd2f1317726459857210716103103dd95e3d
David Oghi <david.oghi@broadshiftconnect.com>
2025-06-27 13:50:41 +0100

    Updates

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -23,18 +23,45 @@ class UsersExport implements WithHeadings, FromQuery
 	 */
 	public function headings(): array
 	{
-		return ["Family ID","Transaction ID", "Registration Status", "Name", "Email", "Phone", "Chapter","Registration Date","Amount Paid","Level","Hostel","Foodstand","Purpose","Location"];
+		return ["Family ID","Transaction ID", "Registration Status", "Name", "Moderator","Email", "Phone", "Chapter","Registration Date","Amount Paid","Level","Hostel","Foodstand","Purpose","Location", "Gender"];
 	}
 	
 	public function query()
 	{
+		// $participants = User::join('payments', 'payments.user_id', '=', 'users.id')
+		// ->leftJoin('hostels', 'hostels.id', '=', 'payments.hostel_id')
+		// ->leftJoin('food', 'food.id', '=', 'payments.food_id')
+		// ->leftJoin('chapters', 'chapters.id', '=', 'users.chapter_id')
+		// ->where(['payments.conference_edition_id'=> $this->data['edition_id']])->where('role','!=',1)
+		// 	->select('users.family_id', 'payments.transid', 'payments.registration_status', 'users.name', 'users.payments.uploaded_by.users.name', 'users.email', 'users.phone', 'chapters.name as chapter', 'payments.created_at as registration_date','payments.amount_paid', 'payments.level','hostels.name as hostel','food.name as foodstand', 'payments.purpose', 'payments.location')
+		// 	->orderBy('users.created_at', 'desc');
 		$participants = User::join('payments', 'payments.user_id', '=', 'users.id')
-		->leftJoin('hostels', 'hostels.id', '=', 'payments.hostel_id')
-		->leftJoin('food', 'food.id', '=', 'payments.food_id')
-		->leftJoin('chapters', 'chapters.id', '=', 'users.chapter_id')
-		->where(['payments.conference_edition_id'=> $this->data['edition_id']])->where('role','!=',1)
-			->select('users.family_id', 'payments.transid', 'payments.registration_status', 'users.name', 'users.email', 'users.phone', 'chapters.name as chapter', 'payments.created_at as registration_date','payments.amount_paid', 'payments.level','hostels.name as hostel','food.name as foodstand', 'payments.purpose', 'payments.location')
-			->orderBy('users.created_at', 'desc');
+			->leftJoin('hostels', 'hostels.id', '=', 'payments.hostel_id')
+			->leftJoin('food', 'food.id', '=', 'payments.food_id')
+			->leftJoin('chapters', 'chapters.id', '=', 'users.chapter_id')
+			->leftJoin('users as uploaded_by_users', 'uploaded_by_users.id', '=', 'payments.uploaded_by')
+			->where('payments.conference_edition_id', $this->data['edition_id'])
+			->where('users.role', '!=', 1)
+			->select(
+				'users.family_id',
+				'payments.transid',
+				'payments.registration_status',
+				'users.name',
+				'uploaded_by_users.name as uploaded_by_name',
+				'users.email',
+				'users.phone',
+				'chapters.name as chapter',
+				'payments.created_at as registration_date',
+				'payments.amount_paid',
+				'payments.level',
+				'hostels.name as hostel',
+				'food.name as foodstand',
+				'payments.purpose',
+				'payments.location',
+				'users.sex'
+			)
+			->orderBy('uploaded_by_users.name')
+			->orderBy('users.created_at', 'asc');
 
 		return $participants;
 	}
--- resources/views/conference_management/admin/editions/utility_index.blade.php
+++ resources/views/conference_management/admin/editions/utility_index.blade.php
@@ -13,7 +13,7 @@
                     <div class="card-content">
                         <div class="card-body card-dashboard">
                             <a href="{{ route('edition.fix.attempted',['edition'=>$edition->id]) }}" class="btn btn-primary mt-1">Fix Attempted Registration ({{$count}})</a>        
-                            {{-- <a href="{{ route('hostels.repair.allocation',['edition'=>$edition->id]) }}" onclick="return confirm('Are you sure?')" class="btn btn-info mt-1">Repair Hostel Allocation</a>    --}}
+                            {{-- <a href="{{ route('edition.fix.attempted',['edition'=>$edition->id]) }}" class="btn btn-info mt-1">Download Consolidate Participants</a>         --}}
                         </div>
                     </div>
                 </div>
