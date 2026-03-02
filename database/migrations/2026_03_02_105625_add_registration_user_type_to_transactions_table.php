30beb2377cd0337c4eb6b59d3df1e1ab13bc8c51
David Oghi <davidoghi@Davids-MacBook-Air.local>
2026-03-02 19:59:02 +0100

    Moderator fixes

--- database/migrations/2026_03_02_105625_add_registration_user_type_to_transactions_table.php
+++ database/migrations/2026_03_02_105625_add_registration_user_type_to_transactions_table.php
@@ -12,43 +12,39 @@ return new class extends Migration
      * Run the migrations.
      */
    public function up(): void
-{
-    Schema::table('transactions', function (Blueprint $table) {
-        $table->string('registration_user_type')
-              ->default('participant')
-              ->after('level');
-    });
+    {
+        if(!Schema::hasColumn('transactions', 'registration_user_type')){
+            Schema::table('transactions', function (Blueprint $table) {
+                $table->string('registration_user_type')
+                    ->default('participant')
+                    ->after('level');
+            });
+        }
 
-    // Schema::table('conference_plans', function (Blueprint $table) {
-    //     $table->string('registration_user_type')
-    //           ->default('participant')
-    //           ->after('level');
-    // });
+        DB::table('transactions')
+        ->whereNull('registration_user_type')
+        ->where('level', 'Moderator')
+        ->update(['registration_user_type' => 'moderator']);
 
-    DB::table('transactions')
-    ->whereNull('registration_user_type')
-    ->where('level', 'Moderator')
-    ->update(['registration_user_type' => 'moderator']);
+        DB::table('transactions')
+        ->whereNull('registration_user_type')
+        ->where('level', 'Participant')
+        ->update(['registration_user_type' => 'participant']);
 
-    DB::table('transactions')
-    ->whereNull('registration_user_type')
-    ->where('level', 'Participant')
-    ->update(['registration_user_type' => 'participant']);
+        DB::table('transactions')
+        ->whereNull('registration_user_type')
+        ->whereIn('level', ['Official'])
+        ->update(['registration_user_type' => 'official']);
+    }
 
-    DB::table('transactions')
-    ->whereNull('registration_user_type')
-    ->whereIn('level', ['Official'])
-    ->update(['registration_user_type' => 'official']);
-}
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('registration_user_type');
+        });
 
-public function down(): void
-{
-    Schema::table('transactions', function (Blueprint $table) {
-        $table->dropColumn('registration_user_type');
-    });
-
-    Schema::table('conference_plans', function (Blueprint $table) {
-        $table->dropColumn('registration_user_type');
-    });
-}
+        Schema::table('conference_plans', function (Blueprint $table) {
+            $table->dropColumn('registration_user_type');
+        });
+    }
 };
