3c378a7657f4a179d47c325315fada8ee7a9d0ca
David Oghi <davidoghi@Davids-MacBook-Air.local>
2026-04-26 23:48:07 +0100

    Updates

--- app/Services/ReportNotificationService.php
+++ app/Services/ReportNotificationService.php
@@ -727,9 +727,9 @@ class ReportNotificationService
         /**
          * Folder: public/reports/{year}/{Month}
          */
-        $relativeDir  = "reports/{$report->year}/{$monthName}";
+        $relativeDir  = "uploads/reports/{$report->year}/{$monthName}";
         $absoluteDir  = env('IMAGE_UPLOAD_PATH').'/'.($relativeDir);
-        
+
         if (!is_dir($absoluteDir)) {
             mkdir($absoluteDir, 0755, true);
         }
